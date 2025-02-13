483880a1ebce86341c2540cf6a972dc35dc09f1f
Malethrion <rvphogir@students.codeinstitute.net>
2025-02-13 21:10:52 +0100

    Added input fields for user to enter house details

--- app.py
+++ app.py
@@ -10,3 +10,12 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 st.title('House Price Prediction Dashboard')
 st.markdown('### Enter house details below to predict its price')
+
+# Input fields for user
+sqft = st.number_input('Square Footage (sqft)', min_value=0)
+bedrooms = st.number_input('Number of Bedrooms', min_value=1)
+bathrooms = st.number_input('Number of Bathrooms', min_value=1)
+
+if st.button('Predict Price'):
+    # Code for prediction will go here
+    pass
