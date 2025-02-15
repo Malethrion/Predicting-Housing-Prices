a2928fa545100bfcc4cb795c1bda0844d82c8ca7
Malethrion <rvphogir@students.codeinstitute.net>
2025-02-15 23:04:27 +0100

    Created multipage.py for handling multiple pages in Streamlit

---
+++ app_pages/multipage.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+class MultiPage:
+    """Class to handle multiple Streamlit pages"""
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+        st.set_page_config(page_title=self.app_name, page_icon=":house:")
+
+    def app_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio("Navigation", self.pages, format_func=lambda page: page["title"])
+        page["function"]()
