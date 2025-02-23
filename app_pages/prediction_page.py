c7a8d8e67bc581992f2a2c9f57ba8fb6fe07019b
Malethrion <rvphogir@students.codeinstitute.net>
2025-02-23 12:03:08 +0100

    Fix Pandas FutureWarnings in prediction_page.py by setting correct dtypes for numerical and categorical features

--- app_pages/prediction_page.py
+++ app_pages/prediction_page.py
@@ -88,30 +88,13 @@ def app():
     SaleType = "WD"  # Most common sale type
     SaleCondition = "Normal"  # Most common sale condition
 
-    # Initialize user_data with all expected numerical and categorical features
-    user_data = pd.DataFrame(columns=expected_features)
-
-    # Set numerical features (default to 0 or median if not provided)
+    # Initialize user_data with correct dtypes for numerical and categorical features
     numerical_features = ['Id', 'MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', 'YearBuilt', 
                          'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 
                          '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 
                          'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces', 
                          'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', 
                          '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold']
-    
-    # Set provided numerical features
-    user_data.loc[0, 'GrLivArea'] = GrLivArea
-    user_data.loc[0, 'OverallQual'] = OverallQual
-    user_data.loc[0, 'GarageCars'] = GarageCars
-    user_data.loc[0, 'YearBuilt'] = YearBuilt
-    user_data.loc[0, 'TotalBsmtSF'] = TotalBsmtSF
-
-    # Default other numerical features to 0
-    for feature in numerical_features:
-        if feature not in ['GrLivArea', 'OverallQual', 'GarageCars', 'YearBuilt', 'TotalBsmtSF']:
-            user_data.loc[0, feature] = 0
-
-    # Set categorical features with hardcoded values
     categorical_features = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 
                            'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 
                            'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 
@@ -119,7 +102,24 @@ def app():
                            'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 
                            'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 
                            'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 'SaleType', 'SaleCondition']
-    
+
+    # Create user_data with appropriate dtypes
+    dtypes = {feat: 'float64' for feat in numerical_features}
+    dtypes.update({feat: 'object' for feat in categorical_features})
+    user_data = pd.DataFrame(columns=list(dtypes.keys()), dtype=dtypes)
+
+    # Set numerical features (default to 0 or median if not provided)
+    user_data.loc[0, 'GrLivArea'] = GrLivArea
+    user_data.loc[0, 'OverallQual'] = OverallQual
+    user_data.loc[0, 'GarageCars'] = GarageCars
+    user_data.loc[0, 'YearBuilt'] = YearBuilt
+    user_data.loc[0, 'TotalBsmtSF'] = TotalBsmtSF
+
+    for feature in numerical_features:
+        if feature not in ['GrLivArea', 'OverallQual', 'GarageCars', 'YearBuilt', 'TotalBsmtSF']:
+            user_data.loc[0, feature] = 0
+
+    # Set categorical features with hardcoded values (no dtype warning since 'object' is used)
     user_data.loc[0, 'MSZoning'] = MSZoning
     user_data.loc[0, 'Street'] = Street
     user_data.loc[0, 'Alley'] = Alley
